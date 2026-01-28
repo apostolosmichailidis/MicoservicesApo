af48334588e1d54c55ab3be09bc04021c1f9cfad
Apo <mihailidis.apostolos@gmail.com>
2026-01-28 22:35:33 +0200

    Add ProductApi

---
+++ Apo.Service.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using Apo.Service.ProductAPI.Models;
+using Apo.Service.ProductAPI.Models.Dto;
+using AutoMapper;
+
+namespace Apo.Service.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMappings() 
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Product, ProductDto>().ReverseMap();
+            });
+;           return mappingConfig;
+        }
+    }
+}
---
+++ Apo.Service.ProductAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Apo.Service.ProductAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object? Result { get; set; }
+        public string? Message { get; set; } = "";
+    }
+}
