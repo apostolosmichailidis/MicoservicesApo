3f9d2193ff0dbe070b021a691a98a5574ec86ae2
Apo <mihailidis.apostolos@gmail.com>
2026-02-05 22:05:59 +0200

    Remove old CouponApi project Remove unused aliases

--- Apo.Services.CouponAPI_V2.Application/Common/Exceptions/NotFoundException.cs
+++ Apo.Services.CouponAPI_V2.Application/Common/Exceptions/NotFoundException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Apo.Services.CouponAPI_V2.Application.Common.Exceptions
+﻿namespace Apo.Services.CouponAPI_V2.Application.Common.Exceptions
 {
     public class NotFoundException : Exception
     {
--- Apo.Services.CouponAPI_V2.Infrastructure/CouponRepository.cs
+++ Apo.Services.CouponAPI_V2.Infrastructure/CouponRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Apo.Services.CouponAPI_V2.Domain;
+﻿using Apo.Services.CouponAPI_V2.Domain;
 using Microsoft.EntityFrameworkCore;
 
 
